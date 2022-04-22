8ef555591c185265613f1d046d0c875fb98f1acf
Konstantin Vankov <19534600+kocko94@users.noreply.github.com>
2022-04-22 11:11:17 +0100

    heartbeat animation

--- src/pages/BootPage.js
+++ src/pages/BootPage.js
@@ -10,17 +10,19 @@ export default class BootPage extends Lightning.Component {
         h: 1080,
         src: Utils.asset('images/background.jpg'),
       },
-      Logo: {
+      LabelShadow: {
+        rect: true,
+        mount: 0.5,
+        w: 1920,
+        h: 100,
+        x: 960,
+        y: 540,
+        color: 0xaa000000,
+      },
+      Label: {
         mount: 0.5,
         x: 960,
         y: 540,
-        src: Utils.asset('images/logo.png'),
-      },
-      Label: {
-        mountX: 0.5,
-        mountY: 1,
-        x: 960,
-        y: 1080,
         text: {
           text: 'Welcome back',
           fontFace: 'Funky',
@@ -34,5 +36,29 @@ export default class BootPage extends Lightning.Component {
     // setTimeout(() => {
     //   Router.navigate(Paths.HOME)
     // }, 2000)
+    this._animateText()
+  }
+
+  _animateText() {
+    this.tag('Label')
+      .animation({
+        duration: 1.5,
+        actions: [
+          {
+            p: 'text.fontSize',
+            v: {
+              0: 54,
+              0.33: 64,
+              0.4: 58,
+              0.5: 64,
+              0.55: 58,
+              0.65: 64,
+              0.88: 66,
+              1: 54,
+            },
+          },
+        ],
+      })
+      .start()
   }
 }
